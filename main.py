7c0f2901c7cbd0c7a62ebdbedd90ecae4eeb1573
Rui Pereira <ruiarpereira15@gmail.com>
2024-03-30 00:39:56 +0000

    Making the code more consistent and intuitive aswell as fixing error on the condition for the most recent date

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import pandas as pd
 import requests
 import json
 import os
+from datetime import datetime
 
 # Load credentials from JSON file
-# with open('config.json') as f:
-with open('new_config.json') as f:
+with open('config.json') as f:
+# with open('new_config.json') as f:
     config = json.load(f)
 
 API_KEY = config['API_KEY']
 USERNAME = config['USERNAME']
 BASE_URL = 'http://ws.audioscrobbler.com/2.0/'
 PATH_EXTRACT = config['path_extracted_file'].replace('{username}', USERNAME)
+LATEST_TRACK_DATE = config['latest_track_date']
+
+if LATEST_TRACK_DATE:
+    date_obj = datetime.strptime(LATEST_TRACK_DATE, '%d %b %Y, %H:%M')
+    UNIX_LATEST_TRACK_DATE = str(int(date_obj.timestamp()))
+else:
+    UNIX_LATEST_TRACK_DATE = None
 
 # Function to fetch user data
 def fetch_user_data():
@@ -26,11 +34,13 @@ def fetch_user_data():
     return data['user']
 
 # Get the total of pages of 200 tracks each for the user
-def get_total_pages():
+def get_total_pages(from_date=None, to_date=None):
     params = {
         'method': 'user.getrecenttracks',
         'user': USERNAME,
         'api_key': API_KEY,
+        'from': from_date,
+        'to': to_date,
         'format': 'json',
         'limit': 200  # Fetch only one track to get total number of pages
     }
@@ -40,30 +50,31 @@ def get_total_pages():
     return total_pages
 
 
-def extract_track_data(fetch_all=False, number_pages=1):
+def extract_track_data(from_date=UNIX_LATEST_TRACK_DATE
+                       , to_date=None
+                       , number_pages=None
+                       , update_config=True):
     """
-    Fetches all data from the last page until the most recent one, 
-    depending on number of pages selected.
+    Default is to fetch data since last extract.
+    
     """
     all_tracks = []
-    total_pages = get_total_pages()
-    if fetch_all:
-        number_pages = total_pages
-
+    total_pages = get_total_pages(from_date=from_date, to_date=to_date)
+    
+    # Define page interval
     page = total_pages
+    if not number_pages:
+        number_pages = total_pages
     page_goal = (total_pages - number_pages) + 1
-
-    with open('config.json') as f:
-        config_data = json.load(f)
-        latest_track_date = config_data.get('latest_track_date')
-
+    
     while page >= page_goal:
         params = {
             'method': 'user.getrecenttracks',
             'user': USERNAME,
             'api_key': API_KEY,
             'format': 'json',
-            # 'from': 
+            'from': from_date,
+            'to': to_date,
             'page': page,
             'extended': 0,
             'limit': 200  # Maximum size by page
@@ -86,15 +97,15 @@ def extract_track_data(fetch_all=False, number_pages=1):
         
         page_most_recent_track = tracks[0].get('date', {}).get('#text', '')
         # Skip page if it has already been extracted
-        if latest_track_date and page_most_recent_track >= latest_track_date:
+        if from_date and from_date >= page_most_recent_track:
             page -= 1
-            page_goal -= 1
+            page_goal = max(page_goal - 1, 1)
             continue
             
         for track in tracks:
             # Check if track is more recent than the latest_track_date
             track_date = track.get('date', {}).get('#text', '')
-            if latest_track_date and track_date >= latest_track_date:
+            if from_date and from_date >= track_date:
                 continue  # Skip if track date is not more recent than latest_track_date
             
             if '@attr' in track and track['@attr'].get('nowplaying') == 'true':
@@ -131,12 +142,14 @@ def extract_track_data(fetch_all=False, number_pages=1):
 
     most_recent_date_track = page_most_recent_track
     print('most_recent_track :',most_recent_date_track)
-    if not latest_track_date or most_recent_date_track >= latest_track_date:
-        config['latest_track_date'] = most_recent_date_track
-
-        # Update json file with the most recent date
-        with open('new_config.json', 'w') as f: # Replace by 'config.json' after
-            json.dump(config, f, indent=4)
+    if update_config:
+        if not LATEST_TRACK_DATE or most_recent_date_track >= LATEST_TRACK_DATE:
+            config['latest_track_date'] = most_recent_date_track
+    
+            # Update json file with the most recent date
+            # with open('new_config.json', 'w') as f: # Replace by 'config.json' after
+            with open('config.json', 'w') as f:
+                json.dump(config, f, indent=4)
 
     return all_tracks
 
@@ -206,17 +219,27 @@ def get_image_text(image_list, size):
             return image['#text']
     return None
 
+def output_csv(df, path, new=False):
+    # If CSV doesn't exist or we want a new CSV, create new CSV. Else append to existing CSV
+    if not os.path.exists(path) or new:
+        print('New CSV')
+        df.to_csv(path, index=False)
+    else:
+        print('Adding to existing CSV')
+        df.to_csv(path, mode='a', index=False, header=False)
+  
+
+NEW_CSV = False
+
 # Fetch track data with duration
-track_data = extract_track_data(fetch_all=False, number_pages=1)
+track_data = extract_track_data(number_pages=2)
 
 # Create DataFrame
 track_df = pd.DataFrame(track_data)
 
-# If CSV doesn't exist, create new CSV. Else append to existing CSV
-if not os.path.exists(PATH_EXTRACT):
-    track_df.to_csv(PATH_EXTRACT, index=False)
-else:
-    track_df.to_csv(PATH_EXTRACT, mode='a', index=False, header=False)
+# Output the CSV
+output_csv(df=track_df, path=PATH_EXTRACT, new=(NEW_CSV or not LATEST_TRACK_DATE))
+
 
 """ List of things to improve:
     - Artist image is blank - Try to get it from MusicBrainz
